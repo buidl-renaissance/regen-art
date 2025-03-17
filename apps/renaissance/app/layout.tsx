09bd41a0373e4296a7bac89b276b10d26a89ad50
John Gulbronson <john@benzinga.com>
2025-03-17 03:39:07 -0400

    add img

--- apps/renaissance/app/layout.tsx
+++ apps/renaissance/app/layout.tsx
@@ -4,6 +4,7 @@ import { StyledComponentsRegistry } from './registry';
 export const metadata = {
   title: 'The Digital Renaissance | A Rebellion on Canvas',
   description: 'Experience the collision of past, present, and future in Detroit\'s creative underground. Art that challenges power, identity, and perception.',
+  image: 'https://nyc3.digitaloceanspaces.com/dpop/images/1742193059847-592079574.jpg',
 };
 
 export default function RootLayout({
